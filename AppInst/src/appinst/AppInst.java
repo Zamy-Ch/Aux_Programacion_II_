c684abe25bdb1c2d08d73b3b19a07f0261353252
Zamy-Ch <zamadycontreras10@gmail.com>
2026-03-02 22:36:33 -0400

    Add files via upload

---
+++ AppInst/src/appinst/AppInst.java
@@ -0,0 +1,8 @@
+package appinst;
+public class AppInst {
+    public static void main(String[] args) {
+        Instrumento in=new Instrumento("n","e","t");
+        System.out.println(in);
+    }
+    
+}
